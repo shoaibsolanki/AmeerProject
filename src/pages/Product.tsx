0f622c916e9c977ed9d07597af52650baa1143b2
mohd shohaib <mohdshohaibchouhan.com>
2025-11-26 21:17:16 +0530

    merge

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -4,7 +4,7 @@ import type { Product } from "../services/Product/Productinterface";
 import { GetProducts } from "../services/Product/Productapi";
 import { AddtoCart } from "../services/Cart/Cartapi";
 import { useAuth } from "../Context/Auth";
-import { IndianRupee } from "lucide-react";
+import { Gem, IndianRupee } from "lucide-react";
 import { useSnackbar } from "notistack";
 import { useCart } from "../Context/CartContext";
 
@@ -36,86 +36,95 @@ const TradeCard: React.FC<
 }) => {
   return (
     <div
-      className={`rounded-xl bg-[#0A0A0A] ${
+      className={`rounded-xl bg-[#0A0A0A] shadow group ${
         isPremium
-          ? "p-6 flex flex-row items-center gap-6 w-full relative"
-          : "p-0"
-      } shadow group`}
-      style={isPremium ? { minHeight: 150 } : undefined}
+          ? 'flex h-[208px] p-4 items-start gap-4 rounded-[16px] border border-[rgba(255,255,255,0.08)] bg-[#0A0A0A]'
+          : 'flex w-[384px] p-4 flex-col  gap-4 rounded-[16px] bg-[#0A0A0A]'
+      }`}
     >
-      {/* Toggle Button (Top Right Only for Premium) */}
+      {/* Top Right Toggle for Premium */}
       {isPremium && (
-        <div className="absolute top-2 right-2 z-20">
+        <div className="absolute top-4 right-4 z-20">
           <ToggelButton active={billingType} setActive={setBillingType} />
         </div>
       )}
 
+      {/* Product Image */}
       <img
         alt="indicator chart"
-        className={`rounded-md object-cover ${
+        src="/Trading.png"
+        className={
           isPremium
-            ? "w-40 h-24 bg-[#222229] mr-4"
-            : "w-full h-32 bg-[#222229] rounded-t-xl"
-        }`}
+            ? "w-[269px] h-[176px] rounded-[8px] mr-4 object-cover bg-[#222229]"
+            : "w-full h-[176px] self-stretch rounded-[8px] "
+        }
       />
 
-      <div
-        className={`flex-1 ${isPremium ? "" : "p-4 flex flex-col"} text-white`}
-      >
-        <div className="flex items-center gap-2 mb-1.5">
-          {isPremium ? (
-            <span className="text-xs text-[#ff4867] font-bold">Premium</span>
-          ) : (
-            <span className="text-xs text-[#33e17c] font-semibold">Free</span>
-          )}
-        </div>
-
-        <h3
-          className={`${
-            isPremium ? "font-bold text-base" : "font-semibold text-base mt-0"
-          }`}
-        >
-          {title}
-        </h3>
-
-        <p
-          className={`text-sm text-gray-300 mt-1 ${
-            isPremium ? "" : "line-clamp-2"
-          }`}
-        >
-          {description}
-        </p>
-
-        <div className="flex items-center">
-          <IndianRupee size={14} /> {productAmount}
-        </div>
-
-        <div className="mt-6 flex gap-3 justify-end">
-          {isPremium ? (
-            <>
-              <button className="rounded px-3 py-1.5 text-sm bg-[#232334] text-[#7aaeeb] border border-[#232334] hover:bg-[#25263c]">
-                View Demo
-              </button>
-              <button
-                className="btn-primary"
-                onClick={() => addToCart(productId)}
-              >
-                Upgrade Premium Access
-              </button>
-            </>
-          ) : (
+      {/* Card Content */}
+      {isPremium ? (
+        <div className="flex-1 text-white">
+          <div className="flex items-center gap-2 mb-1.5">
+            <div className="flex items-center gap-1">
+              {/* Premium Badge */}
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
+                <path d="M15 5.10221L7.5 13.7325L0 5.10221L7.5 4.07104L15 5.10221Z" fill="#CE0045"/>
+                <path d="M15 5.10221L7.5 13.7325V4.07104L15 5.10221Z" fill="#A00031"/>
+                <path d="M11.2647 3.18403L10.3768 5.10198L7.5 3.38639L4.62322 5.10198L3.75 3.18403V1.26611H11.25L11.2647 3.18403Z" fill="#CE0045"/>
+                <path d="M11.2647 3.18403L10.3768 5.10198L7.5 3.38639V1.26611H11.25L11.2647 3.18403Z" fill="#A00031"/>
+                <path d="M10.3768 5.10198H4.62329L7.50007 1.26611L10.3768 5.10198Z" fill="#FE7E52"/>
+                <path d="M10.3768 5.10198H7.5V1.26611L10.3768 5.10198Z" fill="#F53241"/>
+                <path d="M3.75 1.26611L0 5.10192H4.62316L3.75 1.26611Z" fill="#FE7E52"/>
+                <path d="M10.3767 5.10192H14.9999L11.2499 1.26611L10.3767 5.10192Z" fill="#F53241"/>
+                <path d="M10.3768 5.10229L7.50007 13.7326L4.62329 5.10229H10.3768Z" fill="#F53241"/>
+                <path d="M10.3768 5.10229L7.5 13.7326V5.10229H10.3768Z" fill="#CE0045"/>
+              </svg>
+              <span className="text-xs text-[#ff4867] font-bold ml-1">Premium</span>
+            </div>
+          </div>
+          <h3 className="text-[20px] font-bold leading-normal">{title}</h3>
+          <p className="text-[#76757A] text-sm font-medium mt-1 leading-[20px]">
+            {/* {description} */}
+            Experience institutional-grade precision with real-time daily support, resistance, and opening levels that react perfectly with market movement. Fully customizable and built for intraday traders and scalpers and giving structure, clarity, and confidence to take high-probability trades every session.
+          </p>
+          <div className="flex justify-between items-center self-stretch">
+          <div className="flex items-center text-white font-semibold text-[20px] leading-normal mt-4">
+            ₹ {productAmount}
+            <span className="text-gray-400 text-[15px] ml-2">/mo</span>
+          </div>
+          <div className="mt-6 flex gap-3 justify-end">
+            <button className="border border-[#D9D9D9] rounded-[8px] flex px-[24px] py-[6px] justify-center items-center gap-[10px]">
+              View Demo
+            </button>
             <button
-              className="btn-primary"
+              className="flex h-[35px] px-[24px] py-[10px] justify-center items-center gap-[10px] rounded-[8px] bg-[#2563FF]"
               onClick={() => addToCart(productId)}
             >
-              Add To Cart
+              Upgrade Premium Access
             </button>
-          )}
+          </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex flex-col flex-1 px-5 pb-5">
+          <span className=" bg-gradient-to-r from-[#08B7FC] to-[#0050C0] bg-clip-text text-transparent font-inter text-[12px] font-semibold leading-normal">Free</span>
+          <h3 className="text-white font-semibold text-lg mt-2">
+            {title}
+          </h3>
+          <p className="text-sm text-gray-300 mt-2 leading-relaxed">
+            {description}
+          </p>
+          <button
+            className="mt-8 w-full  btn-primary"
+            onClick={() => addToCart(productId)}
+          >
+            Add Product
+          </button>
+        </div>
+      )}
     </div>
   );
 };
+
 
 interface TypeToggelButton {
   active: string;
